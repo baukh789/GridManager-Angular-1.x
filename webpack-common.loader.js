e589465076f830180675d3575dcea90faa16a136
baukh789 <182209508@qq.com>
2019-11-15 11:11:45 +0800

    update webpack config

--- webpack-common.loader.js
+++ webpack-common.loader.js
@@ -1,25 +1,27 @@
-/**
- * @author  https://github.com/silence717
- * @date on 2017/12/12
- */
 const path = require('path');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (srcCodeDir, idDev) => {
-	return [
-		{
-			test: /\.js?$/,
-			loaders: ['babel-loader?{"presets":["es2015"]}'],
-			exclude: /(node_modules|bower_components)/,
-			include: [path.join(__dirname, srcCodeDir)]
-		},
-		{
-			test: /\.css$/,
-            use: ExtractTextWebpackPlugin.extract({
-                use: [{
+module.exports = () => {
+    return [
+        {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader
+                },
+                {
                     loader: 'css-loader',
-                }]
-            })
-		}
-	]
+                    options: {
+                        url: true, // 启用/禁用 url() 处理
+                        sourceMap: true // 启用/禁用 Sourcemaps
+                    }
+                }
+             ]
+        }
+    ]
 };
--- webpack-dev-config.js
+++ webpack-dev-config.js
@@ -1,15 +1,17 @@
-const path = require('path');
 const webpack = require('webpack');
-const genRules = require('./webpack-common.loader');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const getRules = require('./webpack-common.loader');
 const { version } = require('./package.json');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-	// map
-	devtool : 'source-map',  // TODO  http://www.css88.com/doc/webpack2/configuration/devtool/
+    mode: 'development',
+    // map
+    //  http://www.css88.com/doc/webpack2/configuration/devtool/
+    devtool: 'cheap-eval-source-map',
 
-	// 入口文件配置
-	context: path.join(__dirname, "src/"),
+    // 入口文件配置
+    context: path.join(__dirname, 'src/'),
 
 	// 入口文件配置
 	entry: {
@@ -18,29 +20,22 @@ const config = {
 
     // 文件导出的配置
     output:{
-        // path: '/' ,
         filename: "webpack-dev-file/js/app.js",
         // publicPath 对于热替换（HMR）是必须的，让webpack知道在哪里载入热更新的模块（chunk）
         publicPath: "/"
     },
 
-    // 配置模块如何解析
+	// 配置模块如何解析
 	resolve:{
 		extensions: [".js"] //当requrie的模块找不到时,添加这些后缀
-	},
-
-	// 自动补全loader
-	resolveLoader: {
-		moduleExtensions: ['-loader']
 	},
 
     // 以插件形式定制webpack构建过程
     plugins: [
         // 将样式文件 抽取至独立文件内
-        new ExtractTextWebpackPlugin({
+        new MiniCssExtractPlugin({
             filename: 'webpack-dev-file/css/app.css',
-            disable: false,
-            allChunks: true
+            chunkFilename: '[id].css'
         }),
 
         // 配置环境变量
@@ -50,9 +45,10 @@ const config = {
             }
         }),
     ],
+
 	// 处理项目中的不同类型的模块
 	module: {
-		rules: genRules('src', true)
+		rules: getRules()
 	}
 };
 
